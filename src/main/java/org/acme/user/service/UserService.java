a87aba7ac3b00fa478967029ded7b8bb7d8b20c7
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2026-06-25 17:34:19 +0200

    feat: UserService

---
+++ src/main/java/org/acme/user/service/UserService.java
@@ -0,0 +1,55 @@
+package org.acme.user.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.acme.user.entity.UserEntity;
+import org.acme.user.repository.UserRepository;
+
+import java.util.List;
+
+@ApplicationScoped
+public class UserService {
+
+    @Inject
+    UserRepository userRepository;
+
+    // metodi di gestione dei dati dell'utente
+    public List<UserEntity> getAllUsers(){
+        return userRepository.listAll();
+    }
+
+    public UserEntity getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public UserEntity getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public UserEntity getUserByName(String name) {
+        return userRepository.findByName(name);
+    }
+
+    // metodi di gestione degli utenti
+    public UserEntity createUser(UserEntity user){
+        userRepository.persist(user);
+        return user;
+    }
+
+    public UserEntity updateUser(Long id, UserEntity newData){
+        UserEntity entity = userRepository.findById(id);
+        if(entity != null){
+            entity.setName(newData.getName());
+            entity.setEmail(newData.getEmail());
+        }else {
+            throw new IllegalArgumentException("Utente non trovato con id: " + id);
+        }
+
+        return entity;
+    }
+
+    public boolean deleteUser(Long id){
+        return userRepository.deleteById(id);
+    }
+
+}
