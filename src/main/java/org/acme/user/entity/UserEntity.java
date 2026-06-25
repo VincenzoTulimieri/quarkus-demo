3ee8aef90334996e9f1ae4f9ec0f678f4b540bd8
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2026-06-25 17:33:46 +0200

    feat: UserEntity

---
+++ src/main/java/org/acme/user/entity/UserEntity.java
@@ -0,0 +1,42 @@
+package org.acme.user.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public UserEntity(){
+    }
+    public UserEntity(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
