2b885c3aeaf8faa34f96e265980162bab64fe745
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2026-07-14 14:29:46 +0200

    fix: formattazione e tolti gli if

--- src/main/java/org/acme/task/service/TaskService.java
+++ src/main/java/org/acme/task/service/TaskService.java
@@ -3,6 +3,7 @@ package org.acme.task.service;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import org.acme.task.dto.TaskEntityDTO;
 import org.acme.task.entity.TaskEntity;
 import org.acme.task.repository.TaskRepository;
 import org.acme.user.entity.UserEntity;
@@ -29,27 +30,40 @@ public class TaskService {
     }
 
     @Transactional
-    public Optional<TaskEntity> createTask(TaskEntity newTask, Long userId) {
-        Optional<UserEntity> user = userRepository.findByIdOptional(userId);
-        if (user.isEmpty()) {
-            return Optional.empty();
-        }
-        newTask.setUser(user.get());
+    public TaskEntity createTask(TaskEntity newTask, Long userId) {
+        UserEntity user = userRepository.findByIdOptional(userId).orElseThrow(() -> new IllegalArgumentException("Utente non trovate"));
+        newTask.setUser(user);
         newTask.setCreated(LocalDateTime.now());
         taskRepository.persist(newTask);
-        return Optional.of(newTask);
+        return newTask;
     }
 
     @Transactional
-    public Optional<TaskEntity> updateTask(TaskEntity updated, Long taskId) {
-        Optional<TaskEntity> updatedTask = taskRepository.findByIdOptional(taskId);
-        if (updatedTask.isEmpty()) {
-            return Optional.empty();
-        }
-        updated.setTitle(updated.getTitle());
-        updated.setDescription(updated.getDescription());
-        updated.setStatus(updated.getStatus());
-        updated.setDueDate(updated.getDueDate());
-        return Optional.of(updated);
+    public TaskEntityDTO updateTask(TaskEntity updated, Long taskId) {
+        TaskEntity updatedTask = taskRepository.findByIdOptional(taskId).orElseThrow(() -> new IllegalArgumentException("Task non trovate"));
+        updatedTask.setTitle(updated.getTitle());
+        updatedTask.setDescription(updated.getDescription());
+        updatedTask.setStatus(updated.getStatus());
+        updatedTask.setDueDate(updated.getDueDate());
+        return toDTO(updatedTask);
+    }
+
+    @Transactional
+    public boolean deleteTask(Long taskId) {
+        TaskEntity task = taskRepository.findByIdOptional(taskId).orElseThrow(() -> new IllegalArgumentException("Task non trovata"));
+        taskRepository.delete(task);
+        return true;
+    }
+
+    private TaskEntityDTO toDTO(TaskEntity task) {
+        return new TaskEntityDTO(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getStatus(),
+                task.getCreated(),
+                task.getDueDate(),
+                task.getUser().getId()
+        );
     }
 }
